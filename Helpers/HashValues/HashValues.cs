0f89247ebcbf219b40f5861cea6f62c7e5679569
hi.yousef49@gmail.com <hi.yousef49@gmail.com>
2025-05-12 13:35:58 +0300

    Edit HashValues Helper

---
+++ Helpers/HashValues/HashValues.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace CapestoneProject.Helpers.HashValues
+{
+    public static class HashValues
+    {
+        public static string ComputeHashValues(string input)
+        {
+            // Convert string to array of bytes
+            var inputBytes = Encoding.UTF8.GetBytes(input);
+            // Initialization hashing algorithm object
+            var hasher = SHA512.Create(); // Based on BRD doc.
+            // Compute hash
+            var hashedBytes = hasher.ComputeHash(inputBytes);
+            // return the hashed value after convert it into string
+            return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+        }
+
+    }
+}
